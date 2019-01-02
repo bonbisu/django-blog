d8bde00561a5786fdd7b3ff18cbcad9a63002956
bonbisu <fernando.costa30@fatec.sp.gov.br>
2019-01-02 12:52:58 -0200

    :wrench: fix(post_list): change post order

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from .models import Post
 # Create your views here.
 
 def post_list (request):
-    posts = Post.objects.filter(published_at__lte=timezone.now()).order_by('published_at')
+    posts = Post.objects.filter(published_at__lte=timezone.now()).order_by('-published_at')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail (request, pk):
