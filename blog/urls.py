e301d2b2058ecbdcf3c26ebed318c2e9e1f274cc
bonbisu <fernando.costa30@fatec.sp.gov.br>
2019-01-02 17:47:45 -0200

    :star: new(drafts): add draft page

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name = 'post_list/'),
+    path('', views.post_list, name = 'post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/<pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<pk>/delete', views.post_delete, name='post_delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -9,6 +9,10 @@ def post_list (request):
     posts = Post.objects.filter(published_at__lte=timezone.now()).order_by('-published_at')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+def post_draft_list(request):
+    posts = Post.objects.filter(published_at__isnull=True).order_by('-created_at')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
 def post_detail (request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
@@ -19,7 +23,7 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_at = timezone.now()
+            # post.published_at = timezone.now() # w/o this line, post will be saved as draft, not published
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -33,9 +37,21 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_at = timezone.now()
+            # post.published_at = timezone.now() # w/o this line, post will be saved as draft, not published
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    post.save() # not in tutorial
+    return redirect('post_detail', pk=pk)
+
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
+
